0141bca394e3b4ff1c72f5426e3693b6f3e2542d
Corbin Garner <corbingar@gmail.com>
2021-04-03 23:09:00 -0700

    new file:   README.md 	new file:   index.js 	new file:   lib/employee.js 	new file:   lib/manager.js 	new file:   package-lock.json 	new file:   package.json 	new file:   utils/generateHtml.js

---
+++ README.md
@@ -0,0 +1,32 @@
+
+# Readme Generator
+
+## Description
+This project is a readme generator designed to be a fast way to set up a basic no nonesense readme's 
+
+this project accomplishes this through Inquirer creating prompts for the user to answer which are then automatically formatted and put into a basic readme file.
+
+
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+## Installation
+1 clone this repository
+2 perform and npm init and intsall Inquirer
+3 complete
+
+## Usage
+1 node the index.js in a command prompt 
+2 answer the prompts
+3 done your readme should be in the general folder
+
+## Credits
+
+
+## License
+
+
---
+++ index.js
@@ -0,0 +1,97 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+
+const generateHtml = require('./util/generateHtml');
+const engineer = require('./lib/engineer');
+const manager = require('./lib/manager');
+const intern = require('./lib/intern');
+
+// The array of Employees
+var employees = []
+var positions = ["Team Manager","Engineer","Intern"]
+
+// The prompt for creating one employee
+const personPrompt = () =>
+  inquirer.prompt([
+
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the Employees Name?',
+    },
+    {
+      type: 'list',
+      message: 'What is the employees position',
+      name: 'position',
+      choices:  positions,
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is the Employees email?',
+    },
+    {
+      type: 'list',
+      message: 'What is your office number',
+      name: 'officenum',
+      choices:  ['Github Username', 'School', 'Office #'],
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'What is your github username',
+    }
+]);
+
+
+const generateperson = (answers) => 
+  employees[employees.length] = new Employee( answers.name , answers.position, employees.length, answers.email, answers.other1 , answers.other2);
+;
+
+
+const MainPrompt = () =>
+  inquirer.prompt([
+    {
+      type: 'list',
+      message: 'What would you like to do',
+      name: 'path',
+      choices:  ['Add Employee', 'Exit and Generate'],
+    }
+  ]);
+;
+
+
+
+function generateHtml(){
+
+};
+
+
+
+function mainLoop(){
+  MainPrompt().then((answers) => {
+    if(answers.path === "Add Employee"){
+
+      personPrompt().then((answers2) => {
+
+        generateperson(answers2);
+
+      });
+
+      mainLoop();
+
+    }else{
+      generateHtml();
+    }
+  });
+}
+
+
+const init = () => {
+  mainLoop();
+};
+
+init();
+
